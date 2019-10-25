b7eed0a71ec9d998e93c8a18ba185fe31e1430fa
Will Xu <will@glowing.com>
2019-10-25 14:11:30 +0800

    Remove tornado from setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=[
         'threadloop>=1,<2',
         'thrift',
-        'tornado>=4.3',
+        # 'tornado>=4.3',
         'opentracing>=2.1,<3.0',
     ],
     # Uncomment below if need to test with unreleased version of opentracing
